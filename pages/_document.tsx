38aa699e62906451625001cc2c29c247406e9c28
aiborg <lean-architect@proton.me>
2023-03-29 09:52:28 +0000

    Revert "Update _document.tsx"
    This reverts commit a8d067c462b793768729df5037a5411acf54101b.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,17 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <head>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YR87PBXJGC"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-YR87PBXJGC');
-        </script>
-      </head>
+      <Head />
       <body>
         <Main />
         <NextScript />

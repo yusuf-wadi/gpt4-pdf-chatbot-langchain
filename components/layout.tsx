c0b0fca17c1ccab7ae2d72ab0bed7baabf265399
aiborg <lean-architect@proton.me>
2023-04-19 17:13:44 +0000

    start new chat button

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 interface LayoutProps {
   children?: React.ReactNode;
 }
@@ -8,9 +10,11 @@ export default function Layout({ children }: LayoutProps) {
       <header className="container sticky top-0 z-40 bg-white">
         <div className="h-16 border-b border-b-slate-200 py-4">
           <nav className="ml-4 pl-6">
-            <a href="/" className="hover:text-slate-600 cursor-pointer">
-              Start NEW chat
-            </a>
+            <Link href="/">
+              <a className="hover:text-slate-600 cursor-pointer">
+                Start NEW chat
+              </a>
+            </Link>
           </nav>
         </div>
       </header>
